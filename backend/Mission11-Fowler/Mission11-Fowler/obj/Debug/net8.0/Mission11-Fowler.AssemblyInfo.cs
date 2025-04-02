96fa671e0a487d867be4db33fb5b7b52dcd4afef
oliviafowler02 <130636149+oliviafowler02@users.noreply.github.com>
2025-04-02 11:22:09 -0600

    backend

--- backend/Mission11-Fowler/Mission11-Fowler/obj/Debug/net8.0/Mission11-Fowler.AssemblyInfo.cs
+++ backend/Mission11-Fowler/Mission11-Fowler/obj/Debug/net8.0/Mission11-Fowler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11-Fowler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8e8516aaa24763da4b329ff31c8d1f7f553875")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+112284f8499d60f86aaa4ab3f87554e548fc6ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11-Fowler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11-Fowler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
